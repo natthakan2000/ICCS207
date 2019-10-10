50de7207f6bf6d582d21ece2a12525bcbc2dbae6
natthakan2000 <natthakaneuaumpon@gmail.com>
2019-10-10 07:56:01 +0700

    Q2 still bug, Q1 done

--- exercise07/d2b.c
+++ exercise07/d2b.c
@@ -1,4 +1,19 @@
-int main(int num){
-    scanf("%d", &num);
-    
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+    {
+    int num, i, j, binaryNum=0,decimalNum;
+    printf("\n\nConvert Decimal to Binary:\n ");
+    printf("-------------------------\n");
+    printf("Enter a number you want to convert : ");
+    scanf("%d",&num);
+    decimalNum=num;
+    i=1;
+     for(j=num;j>0;j=j/2)
+      {
+       binaryNum=binaryNum+(num%2)*i;
+       i=i*10;
+       num=num/2;
+      }
+    printf("\nThe Binary of %d is %d.\n\n",decimalNum,binaryNum);
 }
---
+++ exercise07/mycp.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char ** argv){
+    FILE *reader, *copy;
+    int readNum;
+    int copyNum;
+    char buffer[100];
+    if (argc != 3){
+        fprintf(stderr, "USAGE: binary-test inputfile outputfile.\n");
+        exit(1);
+    }
+    if ((reader = fopen(argv[1],"rb"))==Null){
+        fprintf(stderr, "open read file error.\n");
+        exit(1);
+    }
+    if((copy=fopen(argv[2],"wb"))==Null){
+        fprintf(stderr, "open write file error.\n");
+        exit(1);
+    }
+    while (feof(reader)==0){
+        if((readNum=fread(buffer,1,100,reader))!=100){
+            if(ferror(reader)!=0){
+                fprintf(stderr, "read file error.\n");
+                exit(1);
+            }else if(feof(reader)!=0);
+        }
+        if ((copyNum=fwrite(buffer,1,numr,copyNum))!= readNum){
+            fprintf(stderr, "write file error.\n");
+            exit(1);
+        }
+    }
+    fclose(reader);
+    fclose(copy);
+    return 0;
+}
