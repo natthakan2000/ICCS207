fe37710c13932139deb4d3d77242d51c038e08cd
natthakan2000 <natthakaneuaumpon@gmail.com>
2019-11-01 12:35:56 +0700

    Just pushing starter pack

---
+++ a3/a3-handout/box/box.c
@@ -0,0 +1,105 @@
+/* Name:
+ * ID:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "box.h"
+
+void createBox(Box **b, int init_cap) {
+    // IMPLEMENT
+}
+
+void insert(Box *b, int elem) {
+    // IMPLEMENT
+}
+
+void removeAll(Box *b, int elem) {
+    // IMPLEMENT
+}
+
+void printBox(Box *b) {
+    // IMPLEMENT
+}
+
+double getMean(Box *b) {
+    // IMPLEMENT
+}
+
+void dealloc(Box **b) {
+    // IMPLEMENT
+}
+
+/*
+TEST: ./box < test.in
+OUTPUT:
+AVG = 4.00
+box1 size = 10
+box2 size = 1
+box1 cap = 1024
+box2 cap = 4
+-- b1
+11
+2
+3
+4
+5
+2
+3
+2
+2
+6
+-- b2
+2
+--
+-- b1 (after remove)
+11
+3
+4
+5
+3
+6
+--
+-- b2 (after remove)
+--
+*/
+int main(int argc, char **argv)
+{
+    Box *b1 = NULL;
+    Box *b2 = NULL;
+
+    createBox(&b1, 1);
+    createBox(&b2, 2);
+
+    int n;
+    scanf("%d", &n);
+    for (int i=0; i<n; i++) {
+        int tmp;
+        scanf("%d",&tmp);
+        insert(b1, tmp);
+    }
+    insert(b2, 2);
+
+    printf("AVG = %0.2f\n",getMean(b1));
+    printf("box1 size = %d\n",b1->size);
+    printf("box2 size = %d\n",b2->size);
+    printf("box1 cap = %d\n",b1->cap);
+    printf("box2 cap = %d\n",b2->cap);
+
+    printf("-- b1\n");
+    printBox(b1);
+    printf("-- b2\n");
+    printBox(b2);
+    printf("--\n");
+
+    removeAll(b1, 2);
+    printf("-- b1 (after remove)\n");
+    printBox(b1);
+    printf("--\n");
+    removeAll(b2, 2);
+    printf("-- b2 (after remove)\n");
+    printBox(b2);
+    printf("--\n");
+
+    dealloc(&b1);
+    dealloc(&b2);
+}
---
+++ a3/a3-handout/det/det.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+double compute_det(int **a, int n) {
+  // implement this
+  return 0.0;
+}
+
+/*
+TEST: ./det < det.in
+OUTPUT:
+-105.00000
+*/
+int main(void) {
+  // implement this
+}
