a5465ecca2511dcfd7e8a211fd25d6735e4c7550
natthakan2000 <natthakaneuaumpon@gmail.com>
2019-11-04 18:01:01 +0700

    Q2 done

--- a3/a3-handout/det/det.c
+++ a3/a3-handout/det/det.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 double compute_det(int **a, int n) {
+  
   if (n==1)
   {
     return a[0][0];
-  }else
-  {
-    
   }
-  return 0.0;
+  else
+  {
+  int c = 0;
+  int d = 0;
+  int one = 1;
+  double determinant=0;
+     int** minor = (int**)malloc(sizeof(int*)*(n-1));
+     for (int i = 0; i < n-1; i++)
+     {
+       minor[i] = (int*) malloc(sizeof(int)*(n-1));
+     }
+     for (int i = 0; i < n; i++)
+     {
+      
+       for (int j = 0; j < n; j++)
+       {
+         if(i!=j){
+         for (int k = 1; k < n; k++)
+         {
+             minor[c][k-1] = a[j][k];
+          }
+          c++; 
+        }        
+      }
+       determinant = determinant + one*(a[i][0]*compute_det(minor,n-1));
+       printf("%f\n",determinant);
+       one = -1*one;
+       c=0;
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+      free(minor[i]);
+    }
+    free(minor);
+  return determinant;
+  }
 }
-
 /*
 TEST: ./det < det.in
 OUTPUT:
@@ -20,19 +51,29 @@ OUTPUT:
 */
 int main(void) {
   int input = 0;
+  
   scanf("%d", &input);
-  int** m = (int**) malloc(sizeof(int*)*m);
+  int** m = (int**) malloc(sizeof(int*)*input);
   for (int i = 0; i < input; i++)
   {
-    m[i] = (int*) malloc(sizeof(int)*m);
+    m[i] = (int*) malloc(sizeof(int)*input);
   }
-  for (int i = 0; i < m; i++)
+  printf("----- meow1 ----\n");
+  for (int i = 0; i < input; i++)
   {
-    for (int j = 0; j < m; j++)
+    for (int j = 0; j < input; j++)
     {
       scanf("%d",&m[i][j]);
     }
-    
+    printf("----- meow2 ----\n");
   }
-  
-}
+  double det = 0;
+  printf("----- meow3 ----\n");
+  det = compute_det(m,input);
+  printf("det = %f\n", det);
+  for (int i = 0; i < input; i++)
+  {
+    free(m[i]);
+  }
+  free(m);
+} 
